3f873070e66fd1a81e64e933a61c587f299641fa
dkalabukhov <daniil.kalabukhov@gmail.com>
2025-06-15 16:17:39 +0300

    Solution for 05-db

--- 05-db/01-crud-tasks/tasks/entities/task.entity.ts
+++ 05-db/01-crud-tasks/tasks/entities/task.entity.ts
@@ -1,1 +1,16 @@
-export class Task {}
+import { Column, Entity, PrimaryGeneratedColumn } from "typeorm";
+
+@Entity('tasks')
+export class Task {
+  @PrimaryGeneratedColumn()
+  id: number
+
+  @Column({ type: 'varchar', length: 255 })
+  title: string
+
+  @Column({ type: 'text'})
+  description: string
+
+  @Column({ type: 'boolean', default: false, name: 'is_completed' })
+  isCompleted: boolean
+}
