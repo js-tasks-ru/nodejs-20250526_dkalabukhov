fab98cf212cec27f4c2ee7feb6f6af81a17ddce2
dkalabukhov <daniil.kalabukhov@gmail.com>
2025-06-19 13:21:08 +0300

    Solution for 06-nosql-db

--- 06-nosql-db/01-mongodb/tasks/dto/create-task.dto.ts
+++ 06-nosql-db/01-mongodb/tasks/dto/create-task.dto.ts
@@ -1,3 +1,11 @@
-import { IsString } from "class-validator";
+import { IsNotEmpty, IsString, Min } from "class-validator";
 
-export class CreateTaskDto {}
+export class CreateTaskDto {
+  @Min(2, { message: 'Название задачи не может быть короче 2 символов' })
+  @IsString({ message: 'Название задачи должно быть строкой' })
+  title: string;
+
+  @IsNotEmpty({ message: 'Описание задачи не может быть пустым' })
+  @IsString({ message: 'Описание задачи должно быть строкой' })
+  description: string;
+}
--- 06-nosql-db/01-mongodb/tasks/schemas/task.schema.ts
+++ 06-nosql-db/01-mongodb/tasks/schemas/task.schema.ts
@@ -1,9 +1,28 @@
 import { Prop, Schema, SchemaFactory } from "@nestjs/mongoose";
 import { HydratedDocument } from "mongoose";
+import { TaskPriority } from "../enums/priority.enum";
 
 export type TaskDocument = HydratedDocument<Task>;
 
 @Schema()
-export class Task {}
+export class Task {
+  @Prop({ unique: true })
+  title: string;
+
+  @Prop()
+  description: string;
+
+  @Prop({ default: false })
+  isCompleted: boolean;
+
+  @Prop()
+  deadline: Date;
+
+  @Prop({
+    type: String,
+    enum: TaskPriority
+  })
+  priority: TaskPriority;
+}
 
 export const TaskSchema = SchemaFactory.createForClass(Task);
